891d808342e7b4e7c1c5fce4388a99655c941526
seggsyurika <110399078+Yurika727@users.noreply.github.com>
2022-09-20 21:14:17 +0700

    Update parallax.js

--- assets/js/parallax.js
+++ assets/js/parallax.js
@@ -5,8 +5,8 @@
     function parallax(e) {
         let _w = window.innerWidth/2;
         let _h = window.innerHeight/2;
-        let _mouseX = e.clientX;
-        let _mouseY = e.clientY;
+        let _mouseX = e.clientX/2;
+        let _mouseY = e.clientY/2;
         let _depth1 = `${50 - (_mouseX - _w) * 0.01}% ${50 - (_mouseY - _h) * 0.01}%`;
         let _depth2 = `${50 - (_mouseX - _w) * 0.02}% ${50 - (_mouseY - _h) * 0.02}%`;
         let _depth3 = `${50 - (_mouseX - _w) * 0.06}% ${50 - (_mouseY - _h) * 0.06}%`;
