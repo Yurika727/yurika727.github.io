7566ab05ce735387c3a5f4ecf094908b45fba86d
seggsyurika <110399078+Yurika727@users.noreply.github.com>
2022-09-20 21:25:50 +0700

    Update parallax.js

--- assets/js/parallax.js
+++ assets/js/parallax.js
@@ -1,17 +1,19 @@
 (function() {
     // Add event listener
     document.addEventListener("mousemove", parallax);
+    const elem = document.querySelector("#parallax");
     // Magic happens here
     function parallax(e) {
         let _w = window.innerWidth/2;
         let _h = window.innerHeight/2;
-        let _mouseX = e.clientX/2;
-        let _mouseY = e.clientY/2;
-        let _depth1 = `${50 - (_mouseX - _w) * 0.01}% ${50 - (_mouseY - _h) * 0.01}%`;
-        let _depth2 = `${50 - (_mouseX - _w) * 0.02}% ${50 - (_mouseY - _h) * 0.02}%`;
-        let _depth3 = `${50 - (_mouseX - _w) * 0.06}% ${50 - (_mouseY - _h) * 0.06}%`;
+        let _mouseX = e.clientX;
+        let _mouseY = e.clientY;
+        let _depth1 = `${50 - (_mouseX - _w) * 0.001}% ${50 - (_mouseY - _h) * 0.001}%`;
+        let _depth2 = `${50 - (_mouseX - _w) * 0.002}% ${50 - (_mouseY - _h) * 0.002}%`;
+        let _depth3 = `${50 - (_mouseX - _w) * 0.006}% ${50 - (_mouseY - _h) * 0.006}%`;
         let x = `${_depth3}, ${_depth2}, ${_depth1}`;
-        document.querySelector("body").style.backgroundPosition = x;
+        console.log(x);
+        elem.style.backgroundPosition = x;
     }
 
 })();
