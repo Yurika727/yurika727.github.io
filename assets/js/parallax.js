8579ad499788d116d419d8411eb4b50e155623a6
seggsyurika <110399078+Yurika727@users.noreply.github.com>
2022-10-16 15:22:19 +0700

    New Update

--- assets/js/parallax.js
+++ assets/js/parallax.js
@@ -7,8 +7,8 @@
         let _mouseX = e.clientX;
         let _mouseY = e.clientY;
         let _depth1 = `${50 - (_mouseX - _w) * 0.01}% ${50 - (_mouseY - _h) * 0.01}%`;
-        let _depth2 = `${50 - (_mouseX - _w) * 0.02}% ${50 - (_mouseY - _h) * 0.02}%`;
-        let _depth3 = `${50 - (_mouseX - _w) * 0.06}% ${50 - (_mouseY - _h) * 0.06}%`;
+        let _depth2 = `${50 - (_mouseX - _w) * 0.02}% ${50 - (_mouseY - _h) * 0.03}%`;
+        let _depth3 = `${50 - (_mouseX - _w) * 0.06}% ${50 - (_mouseY - _h) * 0.08}%`;
         let x = `${_depth3}, ${_depth2}, ${_depth1}`;
         document.querySelector("body").style.backgroundPosition = x;
     }
